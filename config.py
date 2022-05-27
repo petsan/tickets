7a44bd0214a82a5e9d89e55ff929265d87702d93
Peter Sankiewicz <petsan@hotmail.com>
2022-05-26 23:52:48 -0400

    you need to change this file to match your Database IP

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-db_host = os.environ.get('DB_HOST', default='10.0.1.91')
+db_host = os.environ.get('DB_HOST', default='10.0.1.91') # <-- Put your DB IP here
 db_name = os.environ.get('DB_NAME', default='dashboard')
 db_password = os.environ.get('DB_PASSWORD', default='secure_password')
 db_port = os.environ.get('DB_PORT', default='5432')
